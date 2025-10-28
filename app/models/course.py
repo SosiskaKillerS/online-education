500f5ca5f3c3c32e6e09fd7cd7f314b9e7d759fa
SosiskaKillerS <uka.blyatsuka@yandex.ru>
2025-10-28 21:17:49 +0300

    fix sqlalchemy model for course

--- app/models/course.py
+++ app/models/course.py
@@ -1,27 +1,49 @@
-from sqlalchemy import Column, BigInteger, String, Text, ForeignKey
-from app.core.db import Base
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    String,
+    Text,
+    Integer,
+    Numeric,
+    ForeignKey,
+    TIMESTAMP,
+)
 from sqlalchemy.orm import relationship
+
+from app.core.db import Base
 from app.models.user_course import user_course
+
 
 class Course(Base):
     __tablename__ = "course"
-    id = Column(BigInteger, primary_key = True)
-    title = Column(String(255), nullable = False)
-    description = Column(Text, nullable = True)
+
+    id = Column(BigInteger, primary_key=True)
+
+    title = Column(String(255), nullable=False)
+    description = Column(Text, nullable=True)
+
+    study_time = Column(Integer, nullable=True)
+    cost = Column(Numeric(10, 2), nullable=True)
+    format = Column(String(100), nullable=True)
+    education_type = Column(String(100), nullable=True)
+    issued_document = Column(String(255), nullable=True)
+
     category_id = Column(
         BigInteger,
         ForeignKey("category.id"),
-        nullable = False
+        nullable=False,
     )
+
+    created_at = Column(TIMESTAMP(timezone=False), nullable=True)
+    updated_at = Column(TIMESTAMP(timezone=False), nullable=True)
+
     category = relationship(
         "Category",
-        back_populates="courses"
+        back_populates="courses",
     )
 
     users = relationship(
         "User",
         secondary=user_course,
-        back_populates="courses"
+        back_populates="courses",
     )
-
-
