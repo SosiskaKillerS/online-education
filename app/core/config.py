195a8d8d1cb83efbdb5208c1c82154ca64e490d2
SosiskaKillerS <uka.blyatsuka@yandex.ru>
2025-10-28 19:24:56 +0300

    fix import DATABASE_URL from env.example

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
-class DBSettings(BaseSettings):
-    url: str = Field(alias="DATABASE_URL", default="postgresql+asyncpg://user:password@localhost:5432/name")
-
 class Settings(BaseSettings):
+    database_url: str = Field(
+        alias = "DATABASE_URL"
+    )
 
     model_config = SettingsConfigDict(
         env_file=".env.example",
         extra="ignore"
     )
-    db: DBSettings = DBSettings()
+
+    @property
+    def db_url(self)->str:
+        return self.database_url
+
 settings = Settings()
