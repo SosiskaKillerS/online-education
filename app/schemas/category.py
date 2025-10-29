e942ee4e81d68961b08b39357fa34b4c66afa5f7
SosiskaKillerS <uka.blyatsuka@yandex.ru>
2025-10-29 11:50:56 +0300

    add schema for uer and category

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,12 +1,1 @@
-from pydantic import BaseModel
-
-class CategoryBase(BaseModel):
-    name:str
-
-class CategoryRequest(BaseModel):
-    id: int
-    name: str
-
-class CategoryResponse(CategoryBase):
-    pass
 
